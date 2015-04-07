7fe88949872c6650b0490f855c996046b62fad71
Erik Rogers <erik.rogers@live.com>
2015-04-07 19:38:24 -0400

    Added MainWindowViewModel placeholder

---
+++ Spark/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spark.ViewModels
+{
+    public sealed class MainWindowViewModel : WorkspaceViewModel
+    {
+        public MainWindowViewModel(string displayName)
+            : base(displayName) { }
+    }
+}
