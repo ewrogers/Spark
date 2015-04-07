cb40a8115101dbf53052f8eae780bb799185dbe3
Erik Rogers <erik.rogers@live.com>
2015-04-07 19:44:40 -0400

    Moved MainWindow to Views namespace, fixed App startup

--- Spark/App.xaml.cs
+++ Spark/App.xaml.cs
@@ -6,12 +6,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using Spark.ViewModels;
+using Spark.Views;
+
 namespace Spark
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static readonly string ApplicationName = "Spark";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Initialize the main window and view model
+            var window = new MainWindow();
+            var viewModel = new MainWindowViewModel(App.ApplicationName);
+
+            // Bind the request close event to closing the window
+            viewModel.RequestClose += delegate
+            {
+                window.Close();
+            };
+
+            // Assign the view model to the data context and display the main window
+            window.DataContext = viewModel;
+            window.Show();
+        }
     }
 }
--- Spark/Views/MainWindow.xaml.cs
+++ Spark/Views/MainWindow.xaml.cs
@@ -13,11 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Spark
+namespace Spark.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
