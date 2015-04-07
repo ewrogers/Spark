6ea4060050276a86078274e82fb84b38356ab5a8
Erik Rogers <erik.rogers@live.com>
2015-04-07 19:30:21 -0400

    Added WorkspaceViewModel abstract class for closable workspaces

---
+++ Spark/ViewModels/WorkspaceViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+using Spark.Input;
+
+namespace Spark.ViewModels
+{
+    public abstract class WorkspaceViewModel : ViewModelBase
+    {
+        ICommand closeCommand;
+
+        public event EventHandler RequestClose;
+
+        #region Properties
+        public ICommand CloseCommand
+        {
+            get
+            {
+                // Lazy initialized
+                if (closeCommand == null)
+                    closeCommand = new DelegateCommand(parameter => OnRequestClose());
+
+                return closeCommand;
+            }
+        }
+        #endregion
+
+        protected WorkspaceViewModel(string displayName)
+            : base(displayName) { }
+
+        protected virtual void OnRequestClose()
+        {
+            var handler = this.RequestClose;
+
+            if (handler!=null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+}
