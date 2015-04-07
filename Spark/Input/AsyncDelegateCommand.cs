805e33902df7289aa0feab8534f202850576fbe3
Erik Rogers <erik.rogers@live.com>
2015-04-07 19:20:40 -0400

    Added DelegateCommand and AsyncDelegateCommand for UI events

---
+++ Spark/Input/AsyncDelegateCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Spark.Input
+{
+    public sealed class AsyncDelegateCommand : ICommand
+    {
+        readonly Func<object, Task> onExecute;
+        readonly Predicate<object> onCanExecute;
+
+        public AsyncDelegateCommand(Func<object, Task> onExecute)
+            : this(onExecute, null) { }
+
+        public AsyncDelegateCommand(Func<object, Task> onExecute, Predicate<object> onCanExecute)
+        {
+            if (onExecute == null)
+                throw new ArgumentNullException("onExecute");
+
+            this.onExecute = onExecute;
+            this.onCanExecute = onCanExecute;
+        }
+
+        #region ICommand
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public async void Execute(object parameter)
+        {
+            await onExecute(parameter);
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return onCanExecute != null ? onCanExecute(parameter) : true;
+        }
+        #endregion
+    }
+}
---
+++ Spark/Input/DelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+
+namespace Spark.Input
+{
+    public sealed class DelegateCommand : ICommand
+    {
+        readonly Action<object> onExecute;
+        readonly Predicate<object> onCanExecute;
+
+        public DelegateCommand(Action<object> onExecute)
+            : this(onExecute, null) { }
+
+        public DelegateCommand(Action<object> onExecute, Predicate<object> onCanExecute)
+        {
+            if (onExecute == null)
+                throw new ArgumentNullException("onExecute");
+
+            this.onExecute = onExecute;
+            this.onCanExecute = onCanExecute;
+        }
+
+        #region ICommand
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            onExecute(parameter);
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return onCanExecute != null ? onCanExecute(parameter) : true;
+        }
+        #endregion
+    }
+}
