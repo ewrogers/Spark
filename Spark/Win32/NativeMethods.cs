e4332e9440ef0ab6c2c0ea28d0a8b2c0105c9b4c
Erik Rogers <erik.rogers@live.com>
2015-04-08 18:49:57 -0400

    Fixed DLL import name

--- Spark/Win32/NativeMethods.cs
+++ Spark/Win32/NativeMethods.cs
@@ -7,16 +7,16 @@ namespace Spark.Win32
 {
     internal static class NativeMethods
     {
-        [DllImport("kern32", SetLastError = true)]
+        [DllImport("kernel32", SetLastError = true)]
         public static extern Win32ProcessSafeHandle OpenProcess(int processId, bool inheritHandle, Win32ProcessAccess desiredAccess);
         
-        [DllImport("kern32", SetLastError = true)]
+        [DllImport("kernel32", SetLastError = true)]
         public static extern bool ReadProcessMemory(Win32ProcessSafeHandle processHandle, IntPtr baseAddress, byte[] buffer, int count, out int numberOfBytesRead);
 
-        [DllImport("kern32", SetLastError = true)]
+        [DllImport("kernel32", SetLastError = true)]
         public static extern bool WriteProcessMemory(Win32ProcessSafeHandle processHandle, IntPtr baseAddress, byte[] buffer, int count, out int numberOfBytesWritten);
 
-        [DllImport("kern32", SetLastError = true)]
+        [DllImport("kernel32", SetLastError = true)]
         public static extern bool CloseHandle(IntPtr handle);
     }
 }
