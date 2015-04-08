7b7d53a6ed5568b9f01259fbb6b8aaa9b0ab6f38
Erik Rogers <erik.rogers@live.com>
2015-04-08 19:19:40 -0400

    Finished ProcessMemoryStream implementation using Win32 APIs

--- Spark/Interop/ProcessMemoryStream.cs
+++ Spark/Interop/ProcessMemoryStream.cs
@@ -4,21 +4,30 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
+using Spark.Win32;
+
 namespace Spark.Interop
 {
     public class ProcessMemoryStream : Stream
     {
-        bool isDisposed;
-        long position = 0x400000;   // Most processes will map to this base address
-        ProcessAccess processAccess;
+        static readonly int BufferSize = 4 * 1024;              // 4 KB
+        static readonly long ModuleBaseAddress = 0x400000;      // Most processes will map to this base address
 
+        bool isDisposed;
+        Win32ProcessSafeHandle processHandle;
+        byte[] readBuffer = new byte[BufferSize];
+        byte[] writeBuffer = new byte[BufferSize];
+
+        protected long position = ModuleBaseAddress;
+        protected ProcessAccess processAccess;
+        
         #region Stream Properties
         public override bool CanRead
         {
             get
             {
                 CheckIfDisposed();
-                return processAccess.HasFlag(ProcessAccess.Read);
+                return !processHandle.IsClosed && processAccess.HasFlag(ProcessAccess.Read);
             }
         }
 
@@ -27,7 +36,7 @@ namespace Spark.Interop
             get
             {
                 CheckIfDisposed();
-                return true;
+                return !processHandle.IsClosed;
             }
         }
 
@@ -36,7 +45,7 @@ namespace Spark.Interop
             get
             {
                 CheckIfDisposed();
-                return processAccess.HasFlag(ProcessAccess.Write);
+                return !processHandle.IsClosed && processAccess.HasFlag(ProcessAccess.Write);
             }
         }
 
@@ -47,7 +56,11 @@ namespace Spark.Interop
 
         public override long Position
         {
-            get { return position; }
+            get
+            {
+                CheckIfDisposed();
+                return position;
+            }
             set
             {
                 CheckIfDisposed();
@@ -60,15 +73,35 @@ namespace Spark.Interop
         }
         #endregion
 
-        public ProcessMemoryStream(int processID, ProcessAccess desiredAccess = ProcessAccess.ReadWrite)
+        public ProcessMemoryStream(int processId, ProcessAccess desiredAccess = ProcessAccess.ReadWrite)
         {
-             // TODO: open process via Win32 calls
+            if (processId < 0)
+                throw new ArgumentOutOfRangeException("Process ID must be a positive value");
+
+            var win32Flags = Win32ProcessAccess.VmOperation;
+
+            // If read mode was requested, bitwise OR the flag
+            if (desiredAccess.HasFlag(ProcessAccess.Read))
+                win32Flags |= Win32ProcessAccess.VmRead;
+
+            // If write mode was requested, bitwise OR the flag
+            if (desiredAccess.HasFlag(ProcessAccess.Write))
+                win32Flags |= Win32ProcessAccess.VmWrite;
+
+            // Open the process and check if the handle is valid
+            processAccess = desiredAccess;
+            processHandle = NativeMethods.OpenProcess(processId, false, win32Flags);
+
+            if (processHandle.IsInvalid)
+                throw new IOException("Unable to open process");
         }
 
         #region Stream Methods
         public override void Close()
         {
             CheckIfDisposed();
+
+            processHandle.Close();
             base.Close();
         }
 
@@ -81,19 +114,74 @@ namespace Spark.Interop
         public override int Read(byte[] buffer, int offset, int count)
         {
             CheckIfDisposed();
-            throw new NotImplementedException();
+
+            var totalBytesRead = 0;
+
+            while (count > 0)
+            {
+                // Do not exceed the buffer size for each block read
+                var blockSize = Math.Min(count, readBuffer.Length);
+
+                // Read the block from process memory
+                var numberOfBytesRead = 0;
+                var didRead = NativeMethods.ReadProcessMemory(processHandle, (IntPtr)this.Position, readBuffer, blockSize, out numberOfBytesRead);
+
+                // Check if the read was successful
+                if (!didRead || numberOfBytesRead != blockSize)
+                    throw new IOException("Unable to read block from process");
+
+                // Copy the block from the read buffer
+                Buffer.BlockCopy(readBuffer, 0, buffer, offset, blockSize);
+
+                // Increment the offset and stream position by the number of bytes read
+                offset += numberOfBytesRead;
+                this.Position += numberOfBytesRead;
+                totalBytesRead += numberOfBytesRead;
+
+                // Decrement the count by the number of bytes read
+                count -= numberOfBytesRead;
+            }
+
+            return totalBytesRead;
         }
 
         public override int ReadByte()
         {
             CheckIfDisposed();
-            return base.ReadByte();
+
+            // Read the byte from process memory
+            var numberOfBytesRead = 0;
+            var didRead = NativeMethods.ReadProcessMemory(processHandle, (IntPtr)this.Position, readBuffer, 1, out numberOfBytesRead);
+
+            // Check if the read was successful
+            if (!didRead || numberOfBytesRead != 1)
+                throw new IOException("Unable to read byte from process");
+
+            // Increment the stream position by the number of bytes read
+            this.Position += numberOfBytesRead;
+
+            return readBuffer[0];
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
             CheckIfDisposed();
-            throw new NotImplementedException();
+            
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    this.Position = offset;
+                    break;
+
+                case SeekOrigin.Current:
+                    this.Position += offset;
+                    break;
+
+                case SeekOrigin.End:
+                    throw new NotSupportedException("Cannot seek from end of process memory stream");
+            }
+
+            return this.Position;
         }
 
         public override void SetLength(long value)
@@ -105,13 +193,49 @@ namespace Spark.Interop
         public override void Write(byte[] buffer, int offset, int count)
         {
             CheckIfDisposed();
-            throw new NotImplementedException();
+
+            while (count > 0)
+            {
+                // Do not exceed the buffer size for each block written
+                var blockSize = Math.Min(count, writeBuffer.Length);
+
+                // Copy the block to the write buffer
+                Buffer.BlockCopy(buffer, offset, writeBuffer, 0, blockSize);
+
+                // Write block to process memory
+                var numberOfBytesWritten = 0;
+                var didWrite = NativeMethods.WriteProcessMemory(processHandle, (IntPtr)this.Position, writeBuffer, blockSize, out numberOfBytesWritten);
+
+                // Check if the write was successful
+                if (!didWrite || numberOfBytesWritten != blockSize)
+                    throw new IOException("Unable to write block to process");
+
+                // Increment the offset and stream position by the number of bytes written
+                offset += numberOfBytesWritten;
+                this.Position += numberOfBytesWritten;
+
+                // Decrement the count by the number of bytes written
+                count -= numberOfBytesWritten;
+            }
         }
 
         public override void WriteByte(byte value)
         {
             CheckIfDisposed();
-            base.WriteByte(value);
+
+            // Copy value to write buffer
+            writeBuffer[0] = value;
+
+            // Write byte to process memory
+            var numberOfBytesWritten = 0;
+            var didWrite = NativeMethods.WriteProcessMemory(processHandle, (IntPtr)this.Position, writeBuffer, 1, out numberOfBytesWritten);
+
+            // Check if the write was successful
+            if (!didWrite || numberOfBytesWritten != 1)
+                throw new IOException("Unable to write byte to process");
+
+            // Increment the stream position by the number of bytes written
+            this.Position += numberOfBytesWritten;
         }
         #endregion
 
@@ -126,7 +250,8 @@ namespace Spark.Interop
                 // Dispose of managed resources here
             }
 
-            Close();
+            // Dispose of unmanaged resources here
+            processHandle.Dispose();
 
             base.Dispose(isDisposing);
             isDisposed = true;
