8b77aaaffabbc775e105a7b78fe72a2e349c2af7
Erik Rogers <erik.rogers@live.com>
2015-04-08 21:57:36 -0400

    Added SuspendedProcess class for creating processes in a suspended state

---
+++ Spark/Interop/SuspendedProcess.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Spark.Win32;
+
+namespace Spark.Interop
+{
+    public sealed class SuspendedProcess : IDisposable
+    {
+        bool isDisposed;
+        bool resumeOnDispose;
+        Win32ProcessSafeHandle processHandle;
+        Win32ThreadSafeHandle threadHandle;
+        int processId;
+        int threadId;
+
+        #region Properties
+        public int ProcessId { get { return processId; } }
+
+        public int ThreadId { get { return threadId; } }
+
+        public bool IsSuspended { get; private set; }
+        #endregion
+
+        private SuspendedProcess(Win32ProcessInformation processInformation, bool resumeOnDispose)
+        {
+            this.processHandle = processInformation.ProcessHandle;
+            this.threadHandle = processInformation.ThreadHandle;
+            this.processId = processInformation.ProcessId;
+            this.threadId = processInformation.ThreadId;
+
+            this.resumeOnDispose = resumeOnDispose;
+        }
+
+        ~SuspendedProcess()
+        {
+            Dispose(false);
+        }
+
+        public static SuspendedProcess Create(string applicationPath, string commandLine = null, bool resumeOnDispose = true)
+        {
+            // Create the startup info and set the Size parameter to the size of the structure
+            Win32StartupInfo startupInfo = new Win32StartupInfo();
+            startupInfo.Size = Marshal.SizeOf(typeof(Win32StartupInfo));
+
+            Win32ProcessInformation processInformation;
+
+            // Attempt to create the process in a suspended state
+            var didCreate = NativeMethods.CreateProcess(applicationPath,
+                commandLine,
+                IntPtr.Zero,    // NULL
+                IntPtr.Zero,    // NULL
+                false,
+                Win32ProcessCreationFlags.Suspended,
+                IntPtr.Zero,    // NULL
+                null,
+                ref startupInfo,
+                out processInformation);
+
+            // Check if the process was created
+            if (!didCreate)
+                throw new IOException("Unable to create process");
+
+            return new SuspendedProcess(processInformation, resumeOnDispose);
+        }
+
+        public void Resume()
+        {
+            CheckIfDisposed();
+            ResumeProcess();            
+        }
+
+        private void ResumeProcess()
+        {
+            while (NativeMethods.ResumeThread(threadHandle) > 1)
+            {
+                // ResumeThread returns the PREVIOUS suspension count:
+                // -> If it is ZERO, the thread was not suspended.
+                // -> If it is ONE, the thread was suspended and now has been resumed.
+                // -> If it is GREATER THAN ONE, the thread is still suspended.
+            }
+
+            this.IsSuspended = false;
+        }
+
+        #region IDisposable Methods
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(true);
+        }
+
+        void Dispose(bool isDisposing)
+        {
+            if (isDisposed)
+                return;
+
+            if (isDisposing)
+            {
+                // Dispose of managed resources here
+            }
+
+            // Dispose of unmanaged resources here
+            if (resumeOnDispose)
+                ResumeProcess();
+
+            threadHandle.Dispose();
+            processHandle.Dispose();
+            
+            isDisposed = true;
+        }
+
+        void CheckIfDisposed()
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(this.GetType().Name);
+        }
+        #endregion
+    }
+}
