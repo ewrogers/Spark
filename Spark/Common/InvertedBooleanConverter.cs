b56d9921e21c5479d0ff22c4307001f1c9b5a67d
Erik Rogers <erik.rogers@live.com>
2015-04-13 23:35:14 -0400

    Added InvertedBooleanConverter for disabling the client version combo box when Auto Detect is enabled

---
+++ Spark/Common/InvertedBooleanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Spark.Common
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public sealed class InvertedBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Methods
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                throw new ArgumentException("The value must be a boolean value");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+        #endregion
+    }
+}
