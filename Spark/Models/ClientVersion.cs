5029f9ac412588cfeaeaafa32b4ac8c2184bcc3d
Erik Rogers <erik.rogers@live.com>
2015-04-13 22:33:00 -0400

    Added model and view model for ClientVersion

---
+++ Spark/Models/ClientVersion.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Spark.Models
+{
+    [Serializable]
+    public sealed class ClientVersion
+    {
+        #region Standard Client Versions
+        public static readonly ClientVersion Version739 = new ClientVersion()
+        {
+            Name = "7.39",
+            VersionCode = 739,
+            Hash = "ca31b8165ea7409d285d81616d8ca4f2",      // MD5
+            ServerAddressPatchAddress = 0x4341FA,
+            ServerPortPatchAddress = 0x43421D,
+            IntroVideoPatchAddress = 0x42F495,
+            MultipleInstancePatchAddress = 0x05911A9,
+            HideWallsPatchAddress = 0x624BD5
+        };
+        #endregion
+
+        #region Properties
+        public string Name { get; set; }
+        public int VersionCode { get; set; }
+        public string Hash { get; set; }
+        public long ServerAddressPatchAddress { get; set; }
+        public long ServerPortPatchAddress { get; set; }
+        public long IntroVideoPatchAddress { get; set; }
+        public long MultipleInstancePatchAddress { get; set; }
+        public long HideWallsPatchAddress { get; set; }
+        #endregion
+
+        public ClientVersion()
+        {
+
+        }
+
+        #region XML Serialization
+        public XElement ToXElement()
+        {
+            var xml = new XElement("ClientVersion",
+                        new XElement("Name", this.Name),
+                        new XElement("VersionCode", this.VersionCode),
+                        new XElement("Hash", this.Hash),
+                        new XElement("ServerAddressPatchAddress", this.ServerAddressPatchAddress.ToString("X")),
+                        new XElement("ServerPortPatchAddress", this.ServerPortPatchAddress.ToString("X")),
+                        new XElement("IntroVideoPatchAddress", this.IntroVideoPatchAddress.ToString("X")),
+                        new XElement("MultipleInstancePatchAddress", this.MultipleInstancePatchAddress.ToString("X")),
+                        new XElement("HideWallsPatchAddress", this.HideWallsPatchAddress.ToString("X"))
+                    );
+
+            return xml;
+        }
+
+        public static IEnumerable<ClientVersion> FromXElement(XElement xml)
+        {
+            var versions = from x in xml.Descendants("ClientVersion")
+                           select new ClientVersion()
+                           {
+                               Name = (string)x.Element("Name"),
+                               VersionCode = (int)x.Element("VersionCode"),
+                               Hash = (string)x.Element("Hash"),
+                               ServerAddressPatchAddress = ParseHexInteger(x.Element("ServerAddressPatchAddress")),
+                               ServerPortPatchAddress = ParseHexInteger(x.Element("ServerPortPatchAddress")),
+                               IntroVideoPatchAddress = ParseHexInteger(x.Element("IntroVideoPatchAddress")),
+                               MultipleInstancePatchAddress = ParseHexInteger(x.Element("MultipleInstancePatchAddress")),
+                               HideWallsPatchAddress = ParseHexInteger(x.Element("HideWallsPatchAddress"))
+                           };
+
+            return versions;
+        }
+        #endregion
+
+        static long ParseHexInteger(XElement element)
+        {
+            return long.Parse((string)element, NumberStyles.HexNumber, null);
+        }
+    }
+}
---
+++ Spark/ViewModels/ClientVersionViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Spark.Models;
+
+namespace Spark.ViewModels
+{
+    public sealed class ClientVersionViewModel : ViewModelBase
+    {
+        ClientVersion clientVersion;
+
+        #region Model Properties
+        public string Name
+        {
+            get { return clientVersion.Name; }
+            set
+            {
+                OnPropertyChanging();
+                clientVersion.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int VersionCode
+        {
+            get { return clientVersion.VersionCode; }
+            set
+            {
+                OnPropertyChanging();
+                clientVersion.VersionCode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Hash
+        {
+            get { return clientVersion.Hash; }
+            set
+            {
+                OnPropertyChanging();
+                clientVersion.Hash = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long ServerAddressPatchAddress
+        {
+            get { return clientVersion.ServerAddressPatchAddress; }
+            set
+            {
+                OnPropertyChanged();
+                clientVersion.ServerAddressPatchAddress = value;
+                OnPropertyChanging();
+            }
+        }
+
+        public long ServerPortPatchAddress
+        {
+            get { return clientVersion.ServerPortPatchAddress; }
+            set
+            {
+                OnPropertyChanging();
+                clientVersion.ServerPortPatchAddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long IntroVideoPatchAddress
+        {
+            get { return clientVersion.IntroVideoPatchAddress; }
+            set
+            {
+                OnPropertyChanging();
+                clientVersion.IntroVideoPatchAddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long MultipleInstancePatchAddress
+        {
+            get { return clientVersion.MultipleInstancePatchAddress; }
+            set
+            {
+                OnPropertyChanging();
+                clientVersion.MultipleInstancePatchAddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long HideWallsPatchAddress
+        {
+            get { return clientVersion.HideWallsPatchAddress; }
+            set
+            {
+                OnPropertyChanging();
+                clientVersion.HideWallsPatchAddress = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        public ClientVersionViewModel(ClientVersion clientVersion)
+            : base(null)
+        {
+            if (clientVersion == null)
+                throw new ArgumentNullException("clientVersion");
+
+            this.clientVersion = clientVersion;
+        }
+    }
+}
