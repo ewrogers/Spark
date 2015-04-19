c54b2dc0cfc23c9c1215c5c37cd5b838bec3d0cd
Erik Rogers <erik.rogers@live.com>
2015-04-19 01:30:13 -0400

    Added extensions for asynchronous socket and integer values

---
+++ Spark/Extensions/IntegerExtender.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spark.Extensions
+{
+    public static class IntegerExtender
+    {
+        static readonly uint ByteMask = 0xFF;
+        static readonly uint WordMask = 0xFFFF;
+        static readonly uint DoubleWordMask = 0xFFFFFFFF;
+
+        #region Get LoByte Methods
+        public static byte LoByte(this short value) { return (byte)(value & ByteMask); }
+        public static byte LoByte(this ushort value) { return (byte)(value & ByteMask); }
+        #endregion
+
+        #region Get HiByte Methods
+        public static byte HiByte(this short value) { return (byte)((value >> 8) & ByteMask); }
+        public static byte HiByte(this ushort value) { return (byte)((value >> 8) & ByteMask); }
+        #endregion
+
+        #region Get LoWord Methods
+        public static short LoWord(this int value) { return (short)(value & WordMask); }
+        public static ushort LoWord(this uint value) { return (ushort)(value & WordMask); }
+        #endregion
+
+        #region Get HiWord Methods
+        public static short HiWord(this int value) { return (short)((value >> 16) & WordMask); }
+        public static ushort HiWord(this uint value) { return (ushort)((value >> 16) & WordMask); }
+        #endregion
+
+        #region Get LoDoubleWord Methods
+        public static int LoDoubleWord(this long value) { return (int)(value & DoubleWordMask); }
+        public static uint LoDoubleWord(this ulong value) { return (uint)(value & DoubleWordMask); }
+        #endregion
+
+        #region Get HiDoubleWord Methods
+        public static int HiDoubleWord(this long value) { return (int)((value >> 32) & DoubleWordMask); }
+        public static uint HiDoubleWord(this ulong value) { return (uint)((value >> 32) & DoubleWordMask); }
+        #endregion
+
+        #region Make Word Methods
+        public static short MakeWord(byte loByte, byte hiByte) { return (short)((hiByte << 8) | loByte); }
+        public static ushort MakeWordUnsigned(byte loByte, byte hiByte) { return (ushort)((hiByte << 8) | loByte); }
+        #endregion
+
+        #region Make DoubleWord Methods
+        public static int MakeDoubleWord(short loWord, short hiWord) { return ((ushort)hiWord << 16) | (ushort)loWord; }
+        public static uint MakeDoubleWordUnsigned(ushort loWord, ushort hiWord) { return (uint)((hiWord << 16) | loWord); }
+        #endregion
+
+        #region Make QuadWord Methods
+        public static long MakeQuadWord(int loDoubleWord, int hiDoubleWord) { return (long)MakeQuadWordUnsigned((uint)loDoubleWord, (uint)hiDoubleWord); }
+
+        public static ulong MakeQuadWordUnsigned(uint loDoubleWord, uint hiDoubleWord)
+        {
+            var loLong = (ulong)loDoubleWord;
+            var hiLong = (ulong)hiDoubleWord;
+            
+            return ((hiLong << 32) | loLong);
+        }
+        #endregion
+    }
+}
---
+++ Spark/Extensions/SocketExtender.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spark.Extensions
+{
+    // This extension allows Sockets to use the Task asynchronous patterns instead of traditional Begin/End callbacks
+    public static class SocketExtender
+    {
+        public static Task<Socket> AcceptAsync(this Socket socket)
+        {
+            if (socket == null)
+                throw new ArgumentNullException("socket");
+
+            var tcs = new TaskCompletionSource<Socket>();
+
+            socket.BeginAccept(result =>
+            {
+                try
+                {
+                    var s = result.AsyncState as Socket;
+                    var client = s.EndAccept(result);
+
+                    tcs.SetResult(client);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+
+            }, socket);
+
+            return tcs.Task;
+        }
+
+        public static Task ConnectAsync(this Socket socket, IPAddress ipAddress, int port)
+        {
+            if (ipAddress == null)
+                throw new ArgumentNullException("ipAddress");
+
+            if (port <= 0)
+                throw new ArgumentOutOfRangeException("Port must be greater than zero");
+
+            return ConnectAsync(socket, new IPEndPoint(ipAddress, port));
+        }
+
+        public static Task ConnectAsync(this Socket socket, IPEndPoint endpoint)
+        {
+            if (socket == null)
+                throw new ArgumentNullException("socket");
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            socket.BeginConnect(endpoint, result =>
+            {
+                try
+                {
+                    var s = result.AsyncState as Socket;
+                    s.EndConnect(result);
+
+                    tcs.SetResult(s.Connected);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+
+            }, socket);
+
+            return tcs.Task;
+        }
+
+        public static Task DisconnectAsync(this Socket socket, bool reuseSocket = false)
+        {
+            if (socket == null)
+                throw new ArgumentNullException("socket");
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            socket.BeginDisconnect(reuseSocket, result =>
+            {
+                try
+                {
+                    var s = result.AsyncState as Socket;
+                    s.EndDisconnect(result);
+
+                    tcs.SetResult(!s.Connected);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+
+            }, socket);
+
+            return tcs.Task;
+        }
+
+        public static Task<int> ReceiveAsync(this Socket socket, byte[] buffer, int offset, int count, SocketFlags flags = SocketFlags.None)
+        {
+            if (socket == null)
+                throw new ArgumentNullException("socket");
+
+            var tcs = new TaskCompletionSource<int>();
+
+            socket.BeginReceive(buffer, offset, count, flags, result =>
+            {
+                try
+                {
+                    var s = result.AsyncState as Socket;
+                    var numberOfBytesReceived = s.EndReceive(result);
+
+                    tcs.SetResult(numberOfBytesReceived);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+
+            }, socket);
+
+            return tcs.Task;
+        }
+
+        public static Task<int> SendAsync(this Socket socket, byte[] buffer, int offset, int count, SocketFlags flags = SocketFlags.None)
+        {
+            if (socket == null)
+                throw new ArgumentNullException("socket");
+
+            var tcs = new TaskCompletionSource<int>();
+
+            socket.BeginSend(buffer, offset, count, flags, result =>
+            {
+                try
+                {
+                    var s = result.AsyncState as Socket;
+                    var numberOfBytesSent = s.EndSend(result);
+
+                    tcs.SetResult(numberOfBytesSent);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+
+            }, socket);
+
+            return tcs.Task;
+        }
+    }
+}
